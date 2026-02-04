9683f8abac7cb168f82c3c81aa01ac719b035416
d.heiser <derek@volflex.com>
2026-02-03 22:05:03 -0600

    Added permissions check back into the main route to verify there are no longer race conditions with the way MSAL initializes and loads User data from the API.

--- src/ui/src/routes/_auth/jackets/index.tsx
+++ src/ui/src/routes/_auth/jackets/index.tsx
@@ -2,13 +2,20 @@ import { Suspense } from 'react'
 import { createFileRoute, useNavigate } from '@tanstack/react-router'
 import { JacketsListSkeleton } from '@/components/jackets/jackets-list-skeleton'
 import { Button } from '@/components/ui/button.tsx'
+import { UnauthorizedWidget } from '@/components/layout/unauthorized-widget.tsx'
 
 export const Route = createFileRoute('/_auth/jackets/')({
+  beforeLoad: ({ context }) => {
+    return {
+      hasPermission: context.auth.hasPermission
+    }
+  },
   component: RouteComponent,
 })
 
 function RouteComponent() {
   const navigate = useNavigate()
+  const { hasPermission } = Route.useRouteContext()
 
   const handleGoToJacket = () => {
     void navigate({ to: '/jackets/1' })
@@ -16,16 +23,19 @@ function RouteComponent() {
 
   return (
     <>
-      <Suspense fallback={<JacketsListSkeleton />}>
-        <div className="container mx-auto py-6">
-          <div className="flex flex-wrap">
-            <h1 className="text-3xl font-bold mb-6">Job Jacket List</h1>
+      {!hasPermission('Jacket_Read') && <UnauthorizedWidget />}
+      {hasPermission('Jacket_Read') && (
+        <Suspense fallback={<JacketsListSkeleton />}>
+          <div className="container mx-auto py-6">
+            <div className="flex flex-wrap">
+              <h1 className="text-3xl font-bold mb-6">Job Jacket List</h1>
+            </div>
+            <Button variant="default" size="lg" onClick={handleGoToJacket}>
+              Go to Jacket
+            </Button>
           </div>
-          <Button variant="default" size="lg" onClick={handleGoToJacket}>
-            Go to Jacket
-          </Button>
-        </div>
-      </Suspense>
+        </Suspense>
+      )}
     </>
   )
 }
